86f0897509829b1f1637033a012a087df5fc9873
Doual <3242@holbertonschool.com>
2022-01-17 20:18:40 +0100

    betty fixed all

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if not my_list or x == 0:
+        print()
+        return (0)
+    try:
+        for idx in range(x):
+            print(my_list[idx], end="")
+    except:
+        idx = idx - 1
+    finally:
+        print()
+        return(idx + 1)
