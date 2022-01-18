df5186c9cbd1e6063b85b4a8982eaa86b80bf7d8
Doual <3242@holbertonschool.com>
2022-01-18 21:27:52 +0100

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+" Define a class Square Empty "
+
+
+class Square:
+    """
+    Define a classe Square
+    Attribute:
+        size (int): size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
