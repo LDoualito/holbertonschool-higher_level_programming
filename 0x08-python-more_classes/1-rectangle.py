853acb58eb55830d19246ab0b23006a90795594a
Doual <3242@holbertonschool.com>
2022-01-24 20:27:58 +0100

    definition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,34 +3,34 @@
 
 
 class Rectangle:
-    """Definition of the empty class Rectangle """
-    def __init__(self, width=0; height=0):
+    """Constructor of the empty class Rectangle"""
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """private istance attribute : width"""
+        """property for private instance attribute: width"""
         return(self.__width)
 
     @width.setter
     def width(self, value):
-        """width setter"""
+        """setter of width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = ValueError
+            self.__width = value
 
     @property
     def height(self):
-        """private instance attribute : height"""
+        """property for private instance attribute: height"""
         return(self.__height)
 
     @height.setter
     def height(self, value):
-        """height setter"""
+        """setter of height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
