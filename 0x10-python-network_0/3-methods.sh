57f752ba7bba8a02d145946d6f1a2b24d2659601
LDoualito <3242@holbertonschool.com>
2022-04-22 19:43:26 +0200

    fixed lenght

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d ' ' -f2-  
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d ' ' -f2-
