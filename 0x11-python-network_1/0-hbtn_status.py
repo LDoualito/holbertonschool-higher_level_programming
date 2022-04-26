e897a4cdbda1261198d451268b1228600d02d037
LDoualito <3242@holbertonschool.com>
2022-04-26 22:19:10 +0200

    fix newline problem

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,3 +11,4 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(data)))
         print("\t- content: {}".format(data))
         print("\t- utf8 content: {}".format(data.decode("utf-8")))
+        
